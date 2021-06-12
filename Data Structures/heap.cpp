ddb8056e51719817534e6fc3c39014a60c534e87
Veljko28 <charlibear28@gmail.com>
2021-06-12 11:08:19 +0200

    Heap beggining code

---
+++ Data Structures/heap.cpp
@@ -0,0 +1,55 @@
+#include "nodes.h"
+
+// MIN HEAP
+
+template <class T>
+struct heap {
+public:	
+	struct heapnode<T>* root;
+
+	heap()
+	{
+		root = NULL;
+	}
+
+	void add(T data) {
+		struct heapnode<T>* temp = new heapnode<T>;
+		temp->data = data;
+		temp->parent = NULL;
+		temp->left = NULL;
+		temp->right = NULL;
+
+		if (root == NULL) {
+			temp->parent = temp;
+			root = temp;
+		}
+		else {
+			addToHeap(temp);
+		}
+		delete temp;
+	}
+
+	void toString() {
+		struct heapnode<T>* temp = root;
+		while(temp)
+	}
+protected: 
+	// returns true if the new value is bigger than the exisiting one 
+	bool compare(T added,T exisiting){
+		return added > exisiting;
+	}
+
+	void addToHeap(heapnode<T>* temp) {
+		struct heapnode<T>* curr = root;
+		while (true) {
+			if (compare(temp->data, curr->data)) curr = curr->right; // goes right if higher value
+			else curr = curr->left; // goes left otherwise
+
+			if (curr == NULL) {
+				curr = temp;
+				break;
+			}
+		}
+		delete curr;
+	}
+};
