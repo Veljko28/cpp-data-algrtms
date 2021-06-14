eca019d3a04376017395a2069ad1b56432f28252
Veljko28 <charlibear28@gmail.com>
2021-06-14 17:46:41 +0200

    creating a queue with pointers

--- Data Structures/QueueWithPointers.cpp
+++ Data Structures/QueueWithPointers.cpp
@@ -1,7 +1,46 @@
-#include "nodes.h"
 #include <iostream>
 
-class Queue {
+class QueueP {
 private:
+    int* queue;
+    int size;
+
+    void addFront(int value) {
+        for (int i = 1; i<size;i++){
+            queue[i] = queue[i-1];
+        }
+        queue[0] = value;
+    }
+
+public: 
+    QueueP() {
+        size = 0;
+        queue = nullptr;
+    }; 
+    void enqueue(int value) {
+        if (queue == nullptr){
+            queue = (int*)malloc(sizeof(int));
+            queue[0] = value;
+            size++;
+        }
+        else {
+            queue = (int*)realloc(queue, ++size);
+            addFront(value);
+        }
+    }
+    int dequeue(){
+        int temp = queue[size - 1];
+        queue = (int*)realloc(queue, --size);
+        return temp;
+    }
+    void print(){
+        for (int i =0;i<size;i++){
+            std::cout << queue[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+    bool isEmpty(){
+        return size == 0;
+    }
 
 };
