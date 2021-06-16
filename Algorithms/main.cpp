8a9b5ba400a3ec58ad838bce82b624e8f45917c2
Veljko28 <charlibear28@gmail.com>
2021-06-16 10:47:17 +0200

    Testing the Peterson's Algorithm

--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -1,55 +1,18 @@
-#include "Dijkstra.cpp"
+#include "Peterson.cpp"
+#include <thread>
 
 
 int main() {
-	Graph q(9);
+	int val = 0;
+	Peterson p;
+	// make two threads for testing the algorithm and call work function
+	std::thread t1([&] { work(p, val, 0); });
+	std::thread t2([&] { work(p, val, 1); });
 
-	// NODE 0
-	q.addEdge(0, 1, 4);
-	q.addEdge(0, 7, 8);
+	// run the threads
+	t1.join();
+	t2.join();
 
-	// NODE 1
-	q.addEdge(1, 0, 4);
-	q.addEdge(1, 2, 8);
-	q.addEdge(1, 7, 11);
-
-	// NODE 2
-	q.addEdge(2, 1, 8);
-	q.addEdge(2, 8, 2);
-	q.addEdge(2, 5, 4);
-	q.addEdge(2, 3, 7);
-
-	// NODE 3
-	q.addEdge(3, 2, 7);
-	q.addEdge(3, 5, 14);
-	q.addEdge(3, 4, 9);
-
-	// NODE 4
-	q.addEdge(4, 3, 9);
-	q.addEdge(4, 5, 10);
-
-	// NODE 5
-	q.addEdge(5, 6, 2);
-	q.addEdge(5, 2, 4);
-	q.addEdge(5, 3, 14);
-	q.addEdge(5, 4, 10);
-
-	// NODE 6
-	q.addEdge(6, 7, 1);
-	q.addEdge(6, 8, 6);
-	q.addEdge(6, 5, 2);
-
-	// NODE 7
-	q.addEdge(7, 0, 8);
-	q.addEdge(7, 1, 11);
-	q.addEdge(7, 8, 7);
-	q.addEdge(7, 6, 1);
-
-	// NODE 8
-	q.addEdge(8, 2, 2);
-	q.addEdge(8, 7, 7);
-	q.addEdge(8, 6, 6);
-
-	q.findPath(0);
+	std::cout << "FINAL COUNT IS: " << val << std::endl;
 	return 0;
 }
