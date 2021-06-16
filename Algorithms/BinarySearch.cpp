42779dad15f3a23d3f90a3c29ee240179defc1e4
Veljko28 <charlibear28@gmail.com>
2021-06-16 10:47:46 +0200

    Adding the algorithms to their own namespaces

--- Algorithms/BinarySearch.cpp
+++ Algorithms/BinarySearch.cpp
@@ -4,41 +4,42 @@
 using namespace std;
 
 // just use std::binary_search, this is just for practise
+namespace {
+	bool BinarySearch(std::vector<int> v, int value) {
 
-bool BinarySearch(std::vector<int> v, int value) {
+		int low = 0;
+		int high = v.size() - 1;
 
-	int low = 0;
-	int high = v.size() - 1;
+		int mid;
 
-	int mid;
+		while (low <= high)
+		{
+			mid = (low + high) / 2;
 
-	while (low <= high)
-	{
-		mid = (low + high) / 2;
+			if (value == v[mid]) return true;
+			else if (value > v[mid]) low = mid + 1;
+			else high = mid - 1;
+		}
 
-		if (value == v[mid]) return true;
-		else if (value > v[mid]) low = mid + 1;
-		else high = mid - 1;
+		return false;
 	}
 
-	return false;
-}
 
+	bool BinarySearch_Array(int a[], int size, int value) {
+		int low = 0;
+		int high = size - 1;
 
-bool BinarySearch_Array(int a[], int size, int value) {
-	int low = 0;
-	int high = size - 1;
+		int mid;
 
-	int mid;
+		while (low <= high)
+		{
+			mid = (low + high) / 2;
 
-	while (low <= high)
-	{
-		mid = (low + high) / 2;
+			if (value == a[mid]) return true;
+			else if (value > a[mid]) low = mid + 1;
+			else high = mid - 1;
+		}
 
-		if (value == a[mid]) return true;
-		else if (value > a[mid]) low = mid + 1;
-		else high = mid - 1;
+		return false;
 	}
-
-	return false;
 }
