e27c52a89245c9485cd0cb6d10d55abac138eec4
Veljko28 <charlibear28@gmail.com>
2021-06-21 11:08:32 +0200

    Solo Linked List

--- Data Structures/main.cpp
+++ Data Structures/main.cpp
@@ -3,15 +3,79 @@
 #include <string>
 #include <stdlib.h>
 #include <iostream>
+#include "nodes.h"
 //#include "stlRedo.cpp"
- #include "linkedList.cpp"
+ //#include "linkedList.cpp"
 // #include "doublyLinkedList.cpp"
 //#include "QueueWithPointers.cpp"
 
+template <class T>
+class LinkedList {
+private:
+	struct node<T>* head;
+	struct node<T>* tail;
+	int size;
+public:
+	LinkedList()
+	{
+		size = 0;
+		head = NULL;
+		tail = NULL;
+	}
+	bool isEmpty() {
+		return size == 0;
+	}
+
+	void add(T data) {
+		struct node<T>* temp = new node<T>;
+		temp->data = data;
+		temp->next = NULL;
+		if (head == NULL) {
+			head = temp;
+			tail = head;
+			head->next = tail;
+		}
+		else {
+			tail->next = temp;
+			tail = tail->next;
+		}
+		size++;
+	}
+
+	void addRange(std::vector<T> v) {
+		for (T i : v) {
+			add(i);
+		}
+	}
+
+	void toString() {
+		struct node<T>* temp = head;
+		while (temp != NULL) {
+			std::cout << temp->data << " ";
+			temp = temp->next;
+		}
+		std::cout << std::endl;
+	}
+
+
+	void reverse() {
+		struct node<T>* current = head, *next = NULL, *prev = NULL;
+		while (current != NULL)
+		{
+			next = current->next;
+			current->next = prev;
+			prev = current;
+			current = next;
+		}
+		head = prev;
+	}
+};
 
 int main() {
-	LinkedList<int> a;
-	a.addRange({ 3 ,4, 5, 1, 2, 3 });
-	a.toString();
+	LinkedList<int> v;
+	v.addRange({ 3, 4, 5 });
+	v.toString();
+	v.reverse();
+	v.toString();
 	return 0;
 }
