f2114a3f16aeee8d84e571d5e2cf8f1088b5fda3
Veljko28 <charlibear28@gmail.com>
2021-10-12 14:13:16 +0200

    adding the shifting stack problem from codeforces

---
+++ codeforces/ShiftingStack.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+using namespace std;
+
+void ShiftingStack() {
+	long long t;
+	short n;
+	cin >> t;
+	while (t--)
+	{
+		cin >> n;
+		long long s = 0, nd = 0;
+		bool flag = true;
+		vector<int> a(n);
+		for (int& i : a) cin >> i;
+
+		for (int i = 0; i < n; i++) {
+			nd += i;
+			s += a[i];
+			if (s < nd) {
+				cout << "NO" << endl;
+				flag = false;
+				break;
+			}
+		}
+		if (flag) cout << "YES" << endl;
+}
