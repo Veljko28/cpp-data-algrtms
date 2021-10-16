8276380aaf9b0625e34a2480033f584299aa6c75
Veljko28 <charlibear28@gmail.com>
2021-10-16 21:22:44 +0200

    adding the bad boy probelm

---
+++ codeforces/BadBoy.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+using namespace std;
+#define ll long long
+
+
+
+
+void BadBoy()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		long n, m;
+		long c[2];
+		cin >> n >> m >> c[0] >> c[1];
+		cout << 1 << " " << 1 << " " << n << " " << m << '\n';
+	}
+
+}
+
