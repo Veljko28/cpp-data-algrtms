b4477d247475ef9ffc5335dda9bd946d668a2845
Veljko28 <charlibear28@gmail.com>
2021-10-17 19:54:23 +0200

    adding the tests for the last problemc

---
+++ codeforces/TakProg.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+using namespace std;
+#define ll long long
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+}
+
