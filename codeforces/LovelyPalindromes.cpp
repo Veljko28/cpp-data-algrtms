d26b74828d732ffd0224741b59189e2ef638fa16
Veljko28 <charlibear28@gmail.com>
2021-10-22 13:38:04 +0200

    adding lovely palindrome from codeforces

---
+++ codeforces/LovelyPalindromes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+using namespace std;
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+
+void LovelyPalindromes()
+{
+	sync;
+	string s;
+	cin >> s;
+	cout << s;
+	reverse(s.begin(), s.end());
+	cout << s << '\n';
+}
