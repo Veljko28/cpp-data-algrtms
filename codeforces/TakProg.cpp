1ee90bec97410f231c6e7694a76e015d9c601033
Veljko28 <charlibear28@gmail.com>
2021-10-27 19:47:11 +0200

    adjusting main setup

--- codeforces/TakProg.cpp
+++ codeforces/TakProg.cpp
@@ -10,22 +10,23 @@
 #include <iomanip>
 #include <map>
 #include <stack>
+#include <unordered_map>;
 using namespace std;
 #define ll long long
 #define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
-constexpr auto N = 200000;
+constexpr auto N = 1000;
 
 template<typename... T>
 void read(T& ...args) {
 	((cin >> args), ...);
 }
 
-
 int main()
 {
 	sync;
 	
 }
+
 
 
 
