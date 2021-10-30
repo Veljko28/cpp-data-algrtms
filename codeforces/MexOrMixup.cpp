f404b58c5e930ec9e410a2e0905bee99ecc3bbbf
Veljko28 <charlibear28@gmail.com>
2021-10-30 14:47:07 +0200

    mex of mixup update

--- codeforces/MexOrMixup.cpp
+++ codeforces/MexOrMixup.cpp
@@ -18,30 +18,25 @@ constexpr auto N = 1000;
 
 template<typename... T>
 void read(T& ...args) {
-	((cin >> args), ...);
+    ((cin >> args), ...);
 }
 
 
 void MexOrMixup()
 {
-	sync;
+    sync;
     int t;
     cin >> t;
     while (t--) {
-        int a,b;
-        read(a,b);
+        int a, b;
+        read(a, b);
         int c;
-        if (a % 4 == 1) c = a -1;
+        if (a % 4 == 1) c = a - 1;
         else if (a % 4 == 2) c = 1;
         else if (a % 4 == 3) c = a;
         else c = 0;
         if (c == b) cout << a << '\n';
-        else if ((c ^ b) != a) cout << a+1 << '\n';
-        else cout << a+2 << '\n';
+        else if ((c ^ b) != a) cout << a + 1 << '\n';
+        else cout << a + 2 << '\n';
     }
 }
-
-
-
-
-
