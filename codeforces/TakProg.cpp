d1364b2781c44394ccc8c1f87d7290efb9a7bd20
Veljko28 <charlibear28@gmail.com>
2021-10-30 14:46:50 +0200

    adding new macros to the main file

--- codeforces/TakProg.cpp
+++ codeforces/TakProg.cpp
@@ -10,11 +10,19 @@
 #include <iomanip>
 #include <map>
 #include <stack>
-#include <unordered_map>;
+#include <unordered_map>
+#include <deque>
 using namespace std;
 #define ll long long
+#define ull unsigned long long
 #define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
-constexpr auto N = 1000;
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define vi vector<int>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
 
 template<typename... T>
 void read(T& ...args) {
@@ -24,7 +32,7 @@ void read(T& ...args) {
 int main()
 {
 	sync;
-	
+
 }
 
 
