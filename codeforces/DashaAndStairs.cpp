3c099b02208e5cf0ffe4e04aacf04efd8b9d2f9d
Veljko28 <charlibear28@gmail.com>
2021-10-31 19:23:33 +0100

    dasha and stairs from codeforces

---
+++ codeforces/DashaAndStairs.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define vi vector<int>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n;i>=0;i--)
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+void DashaAndStairs()
+{
+	sync;
+	int a, b;
+	read(a, b);
+	if (a == b and b == 0) no;
+	else if (abs(a - b) > 1) {
+		no;
+	}
+	else yes;
+}
+
+
+
+
+
