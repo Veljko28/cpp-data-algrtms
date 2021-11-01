ce37bbd99838d1860e659a8e06bf950bcc679f15
Veljko28 <charlibear28@gmail.com>
2021-11-01 11:52:47 +0100

    adding pilars from codefoces

---
+++ codeforces/Pillars.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define vi vector<int>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+
+void Pillars()
+{
+	sync;
+	int n;
+	cin >> n;
+	vi a(n);
+	vifor(a);
+	int b = *max_element(a.begin(), a.end());
+	int prev = 0; bool max = false;
+	for (int i = 0; i < n; i++) {
+		if (a[i] < prev && !max or (a[i] > prev && max)) {
+			no;
+			return;
+		}
+		prev = a[i];
+		if (a[i] == b) max = true;
+	}
+	yes;
+}
+
+
+
+
+
+
