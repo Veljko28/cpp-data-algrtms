3a09ed4ecb251551d8c155c2dfad18dce46b8654
Veljko28 <charlibear28@gmail.com>
2021-11-02 21:05:55 +0100

    Adding watching a film from codeforces

---
+++ codeforces/WatchingFilms.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>;
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+constexpr auto N = 1000;
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+void WatchingFilms()
+{
+	sync;
+    int n,x,t = 1;
+    int ans = 0;
+    read(n,x);
+    for (int i =0;i<n;i++){
+        int l,r;
+        read(l,r);
+        t += x * ((l-t)/x);
+        ans += r - t + 1;
+        t = r + 1;
+    }
+    cout << ans << '\n';
+}
+
+
+
+
+
