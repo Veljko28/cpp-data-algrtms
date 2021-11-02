cc9b5f619f7623ce39a36ac398be89c5df0a30b2
Veljko28 <charlibear28@gmail.com>
2021-11-02 21:04:55 +0100

    buying a tv from codeforces

---
+++ codeforces/BuyingTV.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>;
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+constexpr auto N = 1000;
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+
+void BuyingTV()
+{
+	sync;
+    ull a,b,x,y;
+    read(a,b,x,y);
+    ull g = __gcd(x,y);
+    x /= g;
+    y /= g;
+    cout << min(a/x,b/y);
+}
+
+
+
+
+
