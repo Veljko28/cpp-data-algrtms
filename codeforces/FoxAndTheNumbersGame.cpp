87b84a46f82d314ae70802fe131b1fc793154bc4
Veljko28 <charlibear28@gmail.com>
2021-11-03 20:06:16 +0100

    Adding fox and hte numbers game from codeforces

---
+++ codeforces/FoxAndTheNumbersGame.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define vi vector<int>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+
+void FoxAndTheNumbersGame()
+{
+	sync;
+	int n;
+	cin >> n;
+	int ans = 0;
+	for (int i = 1; i <= n; i++) {
+		int x;
+		cin >> x;
+		ans = (i == 1 ? x : gcd(ans, x));
+	}
+	cout << ans * n << '\n';
+}
+
+
+
+
+
+
+
+
