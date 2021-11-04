b6e206a5a177a7fcdcd8b227182ade807cb7de62
Veljko28 <charlibear28@gmail.com>
2021-11-04 19:19:14 +0100

    adding chunga changa from codeforces

---
+++ codeforces/ChungaChanga.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define vi vector<int>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+const int maxn = 1e5 + 5;
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+
+
+void ChungaChanga()
+{
+	sync;
+	ull x, y, z, ans = 0;
+	read(x, y, z);
+	ans = (x / z) + (y / z);
+	ull	a = (x + y) / z;
+	if (ans == a) {
+		cout << ans << ' ' << 0 << '\n';
+		return;
+	}
+	cout << a << ' ' << min(z - (x % z), z - (y % z)) << '\n';
+}
