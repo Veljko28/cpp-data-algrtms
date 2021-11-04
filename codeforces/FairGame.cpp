49e042a9044944f4d8b1d6086690de65f9a29a85
Veljko28 <charlibear28@gmail.com>
2021-11-04 19:19:28 +0100

    adding fair game from codeforces

---
+++ codeforces/FairGame.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define vi vector<int>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+const int maxn = 1e5 + 5;
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+
+
+void FairGame()
+{
+	sync;
+	int n;
+	cin >> n;
+	if (n % 2) {
+		no; return;
+	}
+	vi a(n);
+	vifor(a);
+	sort(a.begin(), a.end());
+	if (a[0] == a[n / 2 - 1] and a[n / 2] == a[n - 1] and a[0] != a[n - 1]) {
+		cout << "YES" << '\n';
+		cout << a[0] << ' ' << a[n - 1] << '\n';
+	}
+	else cout << "NO" << '\n';
+}
+
+
+
+
+
+
+
+
