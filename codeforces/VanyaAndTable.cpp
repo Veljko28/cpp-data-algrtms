4439201c85c2994b60d7e0bb736c11599f92ec39
Veljko28 <charlibear28@gmail.com>
2021-11-06 13:52:01 +0100

    adding vanya and table from codeforces

---
+++ codeforces/VanyaAndTable.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define vi vector<int>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+using namespace std;
+const int maxn = 1e5 + 5;
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+
+
+void VanyaAndTable()
+{
+	sync;
+	int n, ans = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x1, y1, x2, y2;
+		read(x1, y1, x2, y2);
+		ans += (x2 - x1 + 1) * (y2 - y1 + 1);
+	}
+	cout << ans << '\n';
+}
