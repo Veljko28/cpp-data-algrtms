e130055c74c84568c565e35d45625a9b08e0be80
Veljko28 <charlibear28@gmail.com>
2021-11-06 13:51:46 +0100

    adding unimodal array from codeforces

---
+++ codeforces/UniModalArray.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define vi vector<int>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+using namespace std;
+const int maxn = 1e5 + 5;
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+
+
+void UniMoralArray()
+{
+	sync;
+	int n;
+	cin >> n;
+	vi a(n);
+	vifor(a);
+	int i = 0, b = 1;
+	for (i; i < n - 1; i++) {
+		if (a[i] < a[i + 1]) b++;
+		else break;
+	}
+	for (i; i < n - 1; i++) {
+		if (a[i] == a[i + 1]) b++;
+		else break;
+	}
+	for (i; i < n - 1; i++) {
+		if (a[i] > a[i + 1]) b++;
+		else break;
+	}
+	if (b == n) yes;
+	else no;
+}
