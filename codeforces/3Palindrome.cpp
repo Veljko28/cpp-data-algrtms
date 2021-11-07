84ab3e8d5a47e68f259598cc2dce89e40b0297f3
Veljko28 <charlibear28@gmail.com>
2021-11-07 14:39:46 +0100

    adding 3 palindrome from codeforces

---
+++ codeforces/3Palindrome.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define vi vector<int>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+using namespace std;
+const int maxn = 1e5 + 5;
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+
+
+void ThreePalindrome()
+{
+	sync;
+	int n, a = 0;
+	char c = 'a';
+	cin >> n;
+	string s;
+	for (int i = 0; i < n; i++) {
+		if (a == 2) {
+			a = 0;
+			c = c == 'a' ? 'b' : 'a';
+		}
+		s += c;
+		a++;
+	}
+	cout << s << '\n';
+}
