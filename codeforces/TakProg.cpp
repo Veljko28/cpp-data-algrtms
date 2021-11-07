e7f183350c7212e06ea678e15b075837d146d549
Veljko28 <charlibear28@gmail.com>
2021-11-07 14:40:52 +0100

    adjusting more macroes

--- codeforces/TakProg.cpp
+++ codeforces/TakProg.cpp
@@ -12,12 +12,10 @@
 #include <stack>
 #include <unordered_map>
 #include <deque>
-using namespace std;
 #define ll long long
 #define ull unsigned long long
 #define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
 #define PI 3.14159265358979323846 
-
 #define pf   printf
 #define yes  pf("YES\n")
 #define no   pf("NO\n")
@@ -28,7 +26,7 @@ using namespace std;
 #define For(n) for(int i=0;i<n;i++)
 #define Forr(n) for (int i=n;i>=0;i--)
 #define vifor(v) for (int& i : v) cin >> i;
-const int maxn = 1e5 + 5;
+using namespace std;
 
 template<typename... T>
 void read(T& ...args) {
@@ -36,9 +34,7 @@ void read(T& ...args) {
 }
 
 
-
-int main()
-{
+int main() {
 	sync;
 	
 }
