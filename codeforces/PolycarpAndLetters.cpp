20702a637be16b467f71a25471232e3a9fd2aff4
Veljko28 <charlibear28@gmail.com>
2021-11-09 16:11:22 +0100

    adding polycarp and letters and codeforces

---
+++ codeforces/PolycarpAndLetters.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define minus pf("-1\n");
+#define vi vector<int>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+using namespace std;
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+
+void PolycarpAndLetters() {
+	sync;
+	int n, mx = 0;
+	cin >> n;
+	string s;
+	cin >> s;
+	For(n) {
+		string a;
+		while (i < n && islower(s[i])) {
+			a += s[i];
+			i++;
+		}
+		set<char> st;
+		for (char c : a) {
+			st.insert(c);
+		}
+		mx = max(mx, (int)st.size());
+	}
+	cout << mx << '\n';
+}
