d7664af406ced7774e1e75fb0815bbae745cbae7
Veljko28 <charlibear28@gmail.com>
2021-11-09 16:11:44 +0100

    adding roman and browser from codeforces

---
+++ codeforces/RomanAndBrowser.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define minus pf("-1\n");
+#define vi vector<int>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+using namespace std;
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+
+void RomanAndBrowser() {
+	sync;
+	int n, k;
+	read(n, k);
+	vi a(n);
+	vifor(a);
+	int s = accumulate(a.begin(), a.end(), 0), ans = INT_MIN;
+	for (int i = 0; i < k; i++) {
+		int sm = s;
+		for (int j = i; j < n; j += k) {
+			sm -= a[j];
+		}
+		ans = max(ans, abs(sm));
+	}
+	cout << ans << '\n';
+}
