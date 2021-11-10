db70fe8ba079085f53bbd216b65fcaa5767bb7f8
Veljko28 <charlibear28@gmail.com>
2021-11-10 17:18:26 +0100

    adding z sort problem from codeforces

---
+++ codeforces/ZSort.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define minus pf("-1\n");
+#define imp pf("Impossible\n");
+#define vi vector<int>
+#define si set<int>
+#define sc set<char>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define NDEBUG
+using namespace std;
+
+string to_string(char c) {
+	return string(1, c);
+}
+
+template<typename T>
+void prv(vector<T> a) {
+	for (int i = 0; i < a.size(); i++) {
+		if (i + 1 == a.size()) cout << a[i] << '\n';
+		else cout << a[i] << ' ';
+	}
+}
+
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
+}
+
+void ZSort() {
+	sync;
+	int n;
+	cin >> n;
+	vi a(n + 1);
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	sort(a.begin() + 1, a.begin() + 1 + n);
+	int b = 1, c = n;
+	for (int i = 1; i <= n; i++) {
+		if (i & 1) cout << a[b++] << ' ';
+		else cout << a[c--] << ' ';
+	}
+}
