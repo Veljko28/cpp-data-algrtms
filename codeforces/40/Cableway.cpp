feeec4ea9e54cd9ad651bbf45df4bd07d15e3678
Veljko28 <charlibear28@gmail.com>
2021-11-20 18:11:30 +0100

    adding cableway from codeforces

---
+++ codeforces/40/Cableway.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define minus pf("-1\n")
+#define imp pf("Impossible\n")
+#define vi vector<int>
+#define vll vector<ll>
+#define si set<int>
+#define sc set<char>
+#define pii pair<int,int>
+#define pci pair<char,int>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+#define vllfor(v) for (ll& i : v) cin >> i;
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define outn(a) cout << a << '\n'
+using namespace std;
+
+template<typename T>
+void prv(vector<T> a) {
+	for (int i = 0; i < a.size(); i++) {
+		if (i + 1 == a.size()) cout << a[i] << '\n';
+		else cout << a[i] << ' ';
+	}
+}
+
+//template<typename... T>
+//void read(T& ...args) {
+//	((cin >> args), ...);
+//}
+
+
+void Cableway() {
+	sync;
+	int r, g, b;
+	cin >> r >> g >> b;
+	int a = 0, t = 0;
+	for (;;) {
+		if (r == g and b == r and r == 0) break;
+		if (a == 0) r -= min(r, 2);
+		else if (a == 1) g -= min(g, 2);
+		else if (a == 2) b -= min(b, 2);
+		a++;
+		if (a == 3) a = 0;
+		t++;
+	}
+	t--;
+	outn(30 + t);
+}
+
+
