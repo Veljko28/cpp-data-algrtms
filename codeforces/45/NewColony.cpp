06efcdd2f9f6785498761d582ebf54a9e676e6e2
Veljko28 <charlibear28@gmail.com>
2021-11-25 16:41:34 +0100

    adding new colony from codeforces

---
+++ codeforces/45/NewColony.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define minus pf("-1\n")
+#define imp pf("Impossible\n")
+#define vi vector<int>
+#define vll vector<ll>
+#define si set<int>
+#define sc set<char>
+#define pii pair<int,int>
+#define pci pair<char,int>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n-1;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+#define vllfor(v) for (ll& i : v) cin >> i;
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define outn(a) cout << a << '\n'
+#define out(a) cout << a << " "
+#define fi first
+#define se second
+using namespace std;
+
+template<typename T>
+void prv(vector<T> a) {
+    for (int i = 0; i < a.size(); i++) {
+        if (i + 1 == a.size()) cout << a[i] << '\n';
+        else cout << a[i] << ' ';
+    }
+}
+
+template<typename T, typename N>
+void prvp(vector<pair<T, N>> a) {
+    for (int i = 0; i < a.size(); i++) {
+        cout << a[i].first << " " << a[i].second << '\n';
+    }
+}
+
+//template<typename... T>
+//void read(T& ...args) {
+//	((cin >> args), ...);
+//}
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+void solve41() {
+    int n;
+    ll k;
+    cin >> n >> k;
+    vi a(n);
+    vifor(a);
+    int mx = *(max_element(all(a)));
+    if (mx * n < k) {
+        outn(-1);
+        return;
+    }
+    int b = n + 1;
+    for (int j = 0; j < k; j++) {
+        int it = -2;
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i] < a[i + 1]) {
+                it = i;
+                break;
+            }
+        }
+        b = it + 1;
+        if (it == -2) break;
+        a[it]++;
+    }
+    outn(b);
+}
+
+void NewColony() {
+    sync;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve41();
+    }
+}
+
+
