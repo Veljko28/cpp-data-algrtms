96da5897677f46a1575751dfec84351c28474580
Veljko28 <charlibear28@gmail.com>
2021-11-30 22:50:05 +0100

    adding muh and sticks from codeforces

---
+++ codeforces/49/MUHAndSticks.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+#include <assert.h>
+#define ui unsigned int
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define minus pf("-1\n")
+#define imp pf("Impossible\n")
+#define vi vector<int>
+#define vs vector<string>
+#define vll vector<ll>
+#define si set<int>
+#define sc set<char>
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pci pair<char,int>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n-1;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+#define vllfor(v) for (ll& i : v) cin >> i;
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define outn(a) cout << a << '\n'
+#define out(a) cout << a << " "
+#define fi first
+#define se second
+#define nmg pf("nemoguce\n");
+#define mg pf("moguce\n");
+using namespace std;
+
+template<typename T>
+void prv(vector<T> a) {
+	for (int i = 0; i < a.size(); i++) {
+		if (i + 1 == a.size()) cout << a[i] << '\n';
+		else cout << a[i] << ' ';
+	}
+}
+
+template<typename T, typename N>
+void prvp(vector<pair<T, N>> a) {
+	for (int i = 0; i < a.size(); i++) {
+		cout << a[i].first << " " << a[i].second << '\n';
+	}
+}
+
+int gcd(int a, int b) {
+	return b == 0 ? a : gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+	return (a * b) / gcd(a, b);
+}
+
+ifstream read(string s) {
+	ifstream file;
+	file.open(s);
+	if (file.fail()) {
+		outn("Failed to Open Input File !");
+		exit(1);
+	}
+	return file;
+}
+
+ofstream write(string s) {
+	ofstream file;
+	file.open(s);
+	if (file.fail()) {
+		outn("Failed to Open Output File !");
+		exit(1);
+	}
+	return file;
+}
+
+
+void MUHAndSticks() {
+	sync;
+	vi a(6);
+	vifor(a);
+	sort(all(a));
+	int l = a[3], b = 0;
+	For(6) {
+		if (a[i] == l) b++;
+	}
+	if (b < 4) {
+		outn("Alien");
+		return;
+	}
+	for (int i = 0; i < 6 and a.size() > 2; i++) {
+		if (a[i] == l) {
+			a.erase(a.begin() + i);
+			i--;
+		}
+	}
+	if (a[0] == a[1]) outn("Elephant");
+	else outn("Bear");
+}
+
