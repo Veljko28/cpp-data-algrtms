b2575baa3f13ba9563582b477a402fb0c2c8267f
Veljko28 <charlibear28@gmail.com>
2021-12-01 21:51:18 +0100

    adding playing with paper from codeforces

---
+++ codeforces/50/PlayingWithPaper.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <deque>
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define minus pf("-1\n")
+#define imp pf("Impossible\n")
+#define vi vector<int>
+#define vll vector<ll>
+#define si set<int>
+#define sc set<char>
+#define pii pair<int,int>
+#define pci pair<char,int>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n-1;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+#define vllfor(v) for (ll& i : v) cin >> i;
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define outn(a) cout << a << '\n'
+#define fi first
+#define se second
+using namespace std;
+
+template<typename T>
+void prv(vector<T> a) {
+	for (int i = 0; i < a.size(); i++) {
+		if (i + 1 == a.size()) cout << a[i] << '\n';
+		else cout << a[i] << ' ';
+	}
+}
+
+template<typename T, typename N>
+void prvp(vector<pair<T, N>> a) {
+	for (int i = 0; i < a.size(); i++) {
+		cout << a[i].first  << " " << a[i].second  << '\n';
+	}
+}
+
+//template<typename... T>
+//void read(T& ...args) {
+//	((cin >> args), ...);
+//}
+
+int gcd(int a, int b) {
+	return b == 0 ? a : gcd(b, a % b);
+}
+
+int lcm(int a, int b){
+	return (a*b)/gcd(a,b);
+}
+
+void PlayingWithPaper() {
+	sync;
+	ll a, b, c=0;
+    cin >> a >> b;
+    while (a > 0 and b > 0){
+        if (a >= b){
+            c += a/b;
+            a %= b;
+        }
+        else {
+            c += b/a;
+            b %= a;
+        }
+    }
+    outn(c);
+}
+
+
