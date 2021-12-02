7650dc2e864a3e3032b028fc37fe69201ae08507
Veljko28 <charlibear28@gmail.com>
2021-12-02 21:52:32 +0100

    adding new macros for multisets

--- codeforces/Source.cpp
+++ codeforces/Source.cpp
@@ -8,68 +8,31 @@
 #include <set>
 #include <sstream>
 #include <iomanip>
-#include <map>
-#include <stack>
-#include <unordered_map>
-#include <deque>
-#define ll long long
-#define ull unsigned long long
-#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
-#define PI 3.14159265358979323846 
-#define pf   printf
-#define yes  pf("YES\n")
-#define no   pf("NO\n")
-#define minus pf("-1\n")
-#define imp pf("Impossible\n")
-#define vi vector<int>
-#define vll vector<ll>
-#define si set<int>
-#define sc set<char>
-#define pii pair<int,int>
-#define pci pair<char,int>
-#define pb push_back
-#define For(n) for(int i=0;i<n;i++)
-#define Forr(n) for (int i=n-1;i>=0;i--)
-#define vifor(v) for (int& i : v) cin >> i;
-#define vllfor(v) for (ll& i : v) cin >> i;
-#define all(c) (c).begin(), (c).end()
-#define sz(x) (int)(x).size()
-#define outn(a) cout << a << '\n'
-#define fi first
-#define se second
 using namespace std;
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
 
-template<typename T>
-void prv(vector<T> a) {
-	for (int i = 0; i < a.size(); i++) {
-		if (i + 1 == a.size()) cout << a[i] << '\n';
-		else cout << a[i] << ' ';
-	}
+template<typename... T>
+void read(T& ...args) {
+	((cin >> args), ...);
 }
 
-template<typename T, typename N>
-void prvp(vector<pair<T, N>> a) {
-	for (int i = 0; i < a.size(); i++) {
-		cout << a[i].first  << " " << a[i].second  << '\n';
-	}
-}
-
-//template<typename... T>
-//void read(T& ...args) {
-//	((cin >> args), ...);
-//}
-
-int gcd(int a, int b) {
-	return b == 0 ? a : gcd(b, a % b);
-}
-
-int lcm(int a, int b){
-	return (a*b)/gcd(a,b);
-}
-
-int main() {
+int main()
+{
 	sync;
-	
+	int n, t;
+	int idx = 1;
+	read(n, t);
+	vector<int> a(n+1);
+	for (int i = 1; i <= n; i++) {
+		int b;
+		cin >> b;
+		a[i] = b;
+	}
+	while (idx < t)
+	{
+		idx += a[idx];
+	}
+	if (idx == t) cout << "Yes" << '\n';
+	else cout << "No" << '\n';
 }
-
-
