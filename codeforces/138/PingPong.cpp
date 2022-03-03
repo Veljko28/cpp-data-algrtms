a1fe338f5b74474055863eb52d19147613b3b1b2
Veljko28 <charlibear28@gmail.com>
2022-03-03 22:06:51 +0100

    adding ping pong from codeforces

---
+++ codeforces/138/PingPong.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+#define sync ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define vi vector<int>
+#define vifor(a) for (int& i : a) cin >> i;
+#define all(a) (a).begin(), (a).end()
+#define ll long long 
+#define pii pair<int,int> 
+#define ff first
+#define ss second
+
+const int mod = 1e9 + 7;
+
+void PingPong() {
+	sync;
+	int t;
+	cin >> t;
+	while (t--) {
+		int a, b;
+		cin >> a >> b;
+		a--;
+		cout << a << ' ' << b << '\n';
+	}
+}
