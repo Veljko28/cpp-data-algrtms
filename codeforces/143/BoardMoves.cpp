564a590557a409720c06c750bae3906912ae5787
Veljko28 <charlibear28@gmail.com>
2022-03-08 23:21:32 +0100

    adding board moves from codeforces

---
+++ codeforces/143/BoardMoves.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+#define sync ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define vi vector<int>
+#define vifor(a) for (int& i : a) cin >> i;
+#define all(a) (a).begin(), (a).end()
+#define ll long long 
+#define pii pair<int,int> 
+#define ff first
+#define ss second
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+
+const int mod = 1e9 + 7;
+
+void BoardMoves() {
+	sync;
+    int t;
+    cin >> t;
+    while (t--){
+        int n;
+        cin >> n;
+        ll ans =0;
+        for (ll i = 1;i<=n/2;i++){
+            ans += i*i;
+        }
+        cout << ans*8 << '\n';
+    }
+}
