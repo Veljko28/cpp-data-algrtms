e5e174cc6ce1cb3fa5c2173e6ce1d9a5b7c00a8b
Veljko28 <charlibear28@gmail.com>
2022-05-01 22:24:43 +0200

    adding even odd game from codeforces

---
+++ codeforces/178/EvenOddGame.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#define ui unsigned int
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define minus pf("-1\n")
+#define imp pf("impossible\n")
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vs vector<string>
+#define vb vector<bool>
+#define vll vector<ll>
+#define si set<int>
+#define sc set<char>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pdd pair<double,double>
+#define pci pair<char,int>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n-1;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+#define vbfor(v) for (bool& i : v) cin >> i;
+#define ovifor(v, n) for (int i =1;i<=n;i++) cin >> v[i]; 
+#define vllfor(v) for (ll& i : v) cin >> i;
+#define vsfor(v) for (string& s : v) cin >> s;
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define outn(a) cout << a << '\n'
+#define out(a) cout << a << " "
+#define ff first
+#define ss second
+#define nmg pf("nemoguce\n");
+#define mg pf("moguce\n");
+#define msi multiset<int> 
+#define mss multiset<string>
+#define min_heap_pq priority_queue<int,vi,greater<int>>
+using namespace std;
+
+template<typename T>
+void prv(vector<T> a) {
+	for (int i = 0; i < a.size(); i++) {
+		if (i + 1 == a.size()) cout << a[i] << '\n';
+		else cout << a[i] << ' ';
+	}
+}
+
+template<typename T, typename N>
+void prvp(vector<pair<T, N>> a) {
+	for (int i = 0; i < a.size(); i++) {
+		cout << a[i].first << " " << a[i].second << '\n';
+	}
+}
+
+ll gcd(ll a, ll b) {
+	return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) {
+	return (a * b) / gcd(a, b);
+}
+
+ifstream read(string s) {
+	ifstream file;
+	file.open(s);
+	if (file.fail()) {
+		outn("Failed to Open Input File !");
+		exit(1);
+	}
+	return file;
+}
+
+ofstream write(string s) {
+	ofstream file;
+	file.open(s);
+	if (file.fail()) {
+		outn("Failed to Open Output File !");
+		exit(1);
+	}
+	return file;
+}
+
+bool customSort(pii a, pii b) {
+	if (a.ss == b.ss) {
+		return a.ff > b.ff;
+	}
+	return a.ss < b.ss;
+}
+
+vector<bool> Eratothenes(ll n) {
+	vector<bool> primes(n + 1, true);
+	primes[0] = primes[1] = false;
+	for (ll i = 2; i * i <= n; i++) {
+		if (primes[i]) {
+			for (ll j = i * i; j <= n; j += i) {
+				primes[j] = false;
+			}
+		}
+	}
+	return primes;
+}
+
+int SumOfDigits(ll n) {
+	int ans = 0;
+	while (n) {
+		ans += n % 10;
+		n /= 10;
+	}
+	return ans;
+}
+
+bool isPalindrome(string s) {
+	string t = s;
+	reverse(all(t));
+	return s == t;
+}
+
+
+void EvenOddGame() {
+	sync;
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vi a, b;
+		for (int i = 0,j; i < n; i++) {
+			cin >> j;
+			if (j % 2) b.push_back(j);
+			else a.push_back(j);
+		}
+		bool ok = 1;
+		sort(all(a),greater<int>()); sort(all(b), greater<int>());
+		int i = 0, j = 0;
+		ll s1 = 0, s2 = 0;
+		while (n--) {
+			if (ok) {
+				if ((i < a.size() and j>=b.size()) or (i<a.size() and j<b.size() and a[i] > b[j])) s1 += a[i++];
+				else j++;
+			}
+			else {
+				if ((j < b.size() and i>=a.size()) or (j<b.size() and i<a.size() and b[j] > a[i])) s2 += b[j++];
+				else i++;
+			}
+			ok ^= 1;
+		}
+		if (s1 == s2) cout << "Tie";
+		else if (s1 > s2) cout << "Alice";
+		else cout << "Bob";
+		cout << '\n';
+	}
+}
