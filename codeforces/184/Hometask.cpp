abcdb948e05b225de705683efe74dac445331433
Veljko28 <charlibear28@gmail.com>
2022-05-07 21:00:54 +0200

    adding hometask from codeforces

---
+++ codeforces/184/Hometask.cpp
@@ -0,0 +1,188 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#define ui unsigned int
+#define ll long long
+#define ull unsigned long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define pf   printf
+#define yes  pf("YES\n")
+#define no   pf("NO\n")
+#define minus pf("-1\n")
+#define imp pf("impossible\n")
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vs vector<string>
+#define vb vector<bool>
+#define vll vector<ll>
+#define si set<int>
+#define sc set<char>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pdd pair<double,double>
+#define pci pair<char,int>
+#define pb push_back
+#define For(n) for(int i=0;i<n;i++)
+#define Forr(n) for (int i=n-1;i>=0;i--)
+#define vifor(v) for (int& i : v) cin >> i;
+#define vbfor(v) for (bool& i : v) cin >> i;
+#define ovifor(v, n) for (int i =1;i<=n;i++) cin >> v[i]; 
+#define vllfor(v) for (ll& i : v) cin >> i;
+#define vsfor(v) for (string& s : v) cin >> s;
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define outn(a) cout << a << '\n'
+#define out(a) cout << a << " "
+#define ff first
+#define ss second
+#define nmg pf("nemoguce\n");
+#define mg pf("moguce\n");
+#define msi multiset<int> 
+#define mss multiset<string>
+#define min_heap_pq priority_queue<int,vi,greater<int>>
+using namespace std;
+
+template<typename T>
+void prv(vector<T> a) {
+	for (int i = 0; i < a.size(); i++) {
+		if (i + 1 == a.size()) cout << a[i] << '\n';
+		else cout << a[i] << ' ';
+	}
+}
+
+template<typename T, typename N>
+void prvp(vector<pair<T, N>> a) {
+	for (int i = 0; i < a.size(); i++) {
+		cout << a[i].first << " " << a[i].second << '\n';
+	}
+}
+
+ll gcd(ll a, ll b) {
+	return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) {
+	return (a * b) / gcd(a, b);
+}
+
+ifstream read(string s) {
+	ifstream file;
+	file.open(s);
+	if (file.fail()) {
+		outn("Failed to Open Input File !");
+		exit(1);
+	}
+	return file;
+}
+
+ofstream write(string s) {
+	ofstream file;
+	file.open(s);
+	if (file.fail()) {
+		outn("Failed to Open Output File !");
+		exit(1);
+	}
+	return file;
+}
+
+
+
+vector<bool> Eratothenes(ll n) {
+	vector<bool> primes(n + 1, true);
+	primes[0] = primes[1] = false;
+	for (ll i = 2; i * i <= n; i++) {
+		if (primes[i]) {
+			for (ll j = i * i; j <= n; j += i) {
+				primes[j] = false;
+			}
+		}
+	}
+	return primes;
+}
+
+int SumOfDigits(ll n) {
+	int ans = 0;
+	while (n) {
+		ans += n % 10;
+		n /= 10;
+	}
+	return ans;
+}
+
+bool customSort(pii a, pii b) {
+	return a.ff < b.ff;
+}
+
+int main() {
+	sync;
+	int n;
+	cin >> n;
+	vi a(n);
+	vifor(a);
+	sort(all(a),greater<int>());
+	if (a.front() == 0) {
+		cout << 0 << '\n';
+		return 0;
+	}
+	if (a.back() != 0) {
+		cout << -1 << '\n';
+		return 0;
+	}
+	int s = accumulate(all(a),0);
+	bool ok = 0;
+	if (s % 3) {
+		int i = n - 1;
+		vi b;
+		while (i >= 0) {
+			if (a[i] == 0) {
+				i--;
+				continue;
+			}
+			if (a[i] % 3 == s % 3) {
+				a.erase(a.begin() + i);
+				ok = 1;
+				break;
+			}
+			else if (a[i] % 3) b.push_back(i);
+			i--;
+		}
+		if (!ok and b.size() >= 2) {
+			a.erase(a.begin() + b[0]);
+			a.erase(a.begin() + b[1]);
+			ok = 1;
+		}
+	}
+	else ok = 1;
+	 
+	if (ok) {
+		if (a.front() == 0) {
+			cout << 0 << '\n';
+			return 0;
+		}
+		for (int i : a) cout << i;
+		cout << '\n';
+	}
+	else cout << -1 << '\n';
+}
