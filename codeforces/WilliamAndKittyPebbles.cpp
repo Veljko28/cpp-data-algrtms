fc6d10b92b14b51a99e2b0c497c11a3e0dae1757
Veljko28 <charlibear28@gmail.com>
2022-11-13 10:22:19 +0100

    adding william and kitty pebbles from codeforces

---
+++ codeforces/WilliamAndKittyPebbles.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vi,greater<int>>
+using namespace std;
+
+
+void solve() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	int mn = INT_MAX;
+	for (int& i : a) {
+		cin >> i;
+		mn = min(mn, i);
+	}
+	ll ans = 0;
+	for (int i = 0; i < n; i++) {
+		ans += a[i] - mn;
+	}
+	cout << ans << '\n';
+}
+
+int main() {
+	sync;
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+}
