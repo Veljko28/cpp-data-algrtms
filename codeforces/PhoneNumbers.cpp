a137aaf5143ff5037bf0d25f24df1d74962ed2ed
Veljko28 <charlibear28@gmail.com>
2022-11-15 12:02:19 +0100

    adding phone numbers from codeforces

---
+++ codeforces/PhoneNumbers.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+
+void solve() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string a, b;
+		cin >> a >> b;
+		int d = 0;
+		for (char c : b) {
+			d += c - 48;
+		}
+		if (d % 2 == 0) cout << a << '\n';
+	}
+}
+
+int main() {
+	sync;
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+}
