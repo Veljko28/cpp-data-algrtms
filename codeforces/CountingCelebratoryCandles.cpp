d89efe280399388963258f3d7a4c85e137486893
Veljko28 <charlibear28@gmail.com>
2022-11-15 12:02:09 +0100

    adding counting celebratory candles from codeforces

---
+++ codeforces/CountingCelebratoryCandles.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vi,greater<int>>
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+    cout << count(all(s), '1');
+}
+
+int main() {
+	sync;
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+}
