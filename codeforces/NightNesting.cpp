00848d04374cc1acaf8b04a0c490225d33992c41
Veljko28 <charlibear28@gmail.com>
2022-11-20 20:48:43 +0100

    adding night nesting from codeforces

---
+++ codeforces/NightNesting.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+using namespace std;
+
+
+void solve() {
+	ll n, m;
+	cin >> n >> m;
+	cout << (n / m) + (n % m ? 1 : 0) << '\n';
+}
+
+int main() {
+	sync;
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+}
