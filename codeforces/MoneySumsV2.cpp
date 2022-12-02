561de013508b164039456fb42df40c0450f9a4e2
Veljko28 <charlibear28@gmail.com>
2022-12-02 23:00:08 +0100

    adding money sums second way from cses

---
+++ codeforces/MoneySumsV2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+ 
+ 
+void solve() {
+	int n;
+	cin >> n;
+	multiset<int> prev = { 0 }, curr = { 0 };
+	for (int i = 0, j; i < n; i++) {
+		cin >> j;
+		for (int c : curr) {
+			if (!curr.count(j + c)) {
+				prev.insert(j + c);
+			}
+		}
+		curr = prev;
+	}
+	cout << curr.size()-1 << '\n';
+	for (int i : curr) {
+		if (!i) continue;
+		cout << i << ' ';
+	}
+	cout << '\n';
+}
+ 
+int main() {
+	sync;
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+}
