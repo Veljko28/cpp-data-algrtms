eeb2b7353d0cce42a126cc39dd671d0594702026
Veljko28 <charlibear28@gmail.com>
2022-12-18 20:59:32 +0100

    prvi

--- kval2/prvi.cpp
+++ kval2/prvi.cpp
@@ -1,1 +1,50 @@
-// code
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+void solve() {
+    int a, b, x, y;
+    cin >> a >> b >> x >> y;
+    int d = a - x, f = b - y;
+    if (d > 0 and f > 0) cout << "?\n";
+    else if (d > 0) cout << "prvi\n";
+    else if (f > 0) cout << "drugi\n";
+    else cout << "nijedan\n";
+}
+
+
+int main() {
+    sync;
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
