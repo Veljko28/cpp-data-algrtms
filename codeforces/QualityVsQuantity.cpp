7bce11d4506ae725e65fcc674a9f86eb20f71341
Veljko28 <charlibear28@gmail.com>
2023-01-11 20:54:48 +0100

    adding quality vs quantity from codeforces

---
+++ codeforces/QualityVsQuantity.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll& i : a) cin >> i;
+    sort(all(a));
+    int l = 0, r = n - 1;
+    ll blue = 0, red = a[n - 1];
+    while (l < r) {
+        blue += a[l];
+        if ((l + 1) > (n - r) and red > blue) {
+            cout << "YES\n";
+            return;
+        }
+        if (blue >= red) red += a[--r];
+        l++; 
+    }
+    cout << "NO\n";
+}
+
+int main() {
+    sync;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
