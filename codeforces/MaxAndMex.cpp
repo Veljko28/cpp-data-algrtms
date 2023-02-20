085e25d81a1650626b799059325a3466140f8997
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-02-20 20:46:56 +0100

    Create MaxAndMex.cpp

---
+++ codeforces/MaxAndMex.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    set<int> s;
+    int mx = 0;
+    for (int i = 0, j; i < n; i++) {
+        cin >> j;
+        s.insert(j);
+        mx = max(mx, j);
+    }
+    int mex = 0;
+    if (!k) {
+        cout << s.size() << '\n';
+        return;
+    }
+    for (int i : s) {
+        if (i == mex) mex++;
+        else break;
+    }
+    if (mex > mx) {
+        cout << s.size() + k;
+    }
+    else {
+        if (s.find((mex + mx + 1) / 2) != s.end()) cout << s.size();
+        else cout << s.size() + 1;
+    }
+    cout << '\n';
+}
+
+int main() {
+    sync;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
