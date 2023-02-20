d43f79a211cd7f30fccc0c80a65c273050fa0a65
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-02-20 20:46:33 +0100

    Create Heating.cpp

---
+++ codeforces/Heating.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+void solve() {
+    ll a, b;
+    cin >> a >> b;
+    ll c = b / a;
+    ll d = b % a;
+    cout << (a - d) * (c*c) + (d) * (c + 1)*(c+1) << '\n';
+}
+
+
+int main() {
+    sync;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
