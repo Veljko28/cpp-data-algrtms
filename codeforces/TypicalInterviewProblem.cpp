4cb976eb2f72e442cd80a990c88d9aef33ca0f9e
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-02-28 19:49:05 +0100

    Create TypicalInterviewProblem.cpp

---
+++ codeforces/TypicalInterviewProblem.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string a = "FBFFBFFB";
+    bool ok = 0;
+    for (int i = 0; i < a.size(); i++) {
+        bool k = 1;
+        int l = i;
+        for (int j = 0; j < n; j++) {
+            if (s[j] != a[l++ % a.size()]) {
+                k = 0;
+            }
+        }
+        if (k) ok = 1;
+   }
+
+    if (ok) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+
+int main() {
+    sync;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
