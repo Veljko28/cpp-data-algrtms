55bc50a1aae5f535e5b20c795d59efbc10e9fc48
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-03-01 19:57:06 +0100

    Update drugi.cpp

--- takprog2/drugi.cpp
+++ takprog2/drugi.cpp
@@ -1,1 +1,57 @@
-// code
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+#define sync ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define all(a) (a).begin(), (a).end()
+#define ll long long
+
+
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+	vector<int> b(n);
+	map<int, vector<int>> m;
+	for (int i = 0,j; i < n; i++) {
+		cin >> j;
+		if (!m[j].size()) {
+			m[j].resize(2);
+		}
+		m[j][i % 2]++;
+	}
+	for (int& i : b) cin >> i;
+
+	bool ok = 1;
+
+	for (int i = 0; i < n; i++) {
+		bool k = 0;
+		if (m[b[i]].size() and m[b[i]][i % 2]) {
+			m[b[i]][i % 2]--;
+			k = 1;
+		}
+		else if (m[-b[i]].size() and m[-b[i]][i % 2 ? 0 : 1]) {
+			m[-b[i]][i % 2 ? 0 : 1]--;
+			k = 1;
+		}
+
+		if (!k) {
+			ok = 0;
+			break;
+		}
+	}
+
+	if (ok) cout << "DA\n";
+	else cout << "NE\n";
+}
+
+int main() {
+	sync;
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+}
