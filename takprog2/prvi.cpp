43bea9447602170227e9e59839186b9e35d7d4ff
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-03-01 19:56:40 +0100

    Update prvi.cpp

--- takprog2/prvi.cpp
+++ takprog2/prvi.cpp
@@ -1,1 +1,32 @@
-// code
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+#define sync ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define all(a) (a).begin(), (a).end()
+#define ll long long
+
+
+using namespace std;
+
+int main() {
+	sync;
+	int n;
+	cin >> n;
+	vector<int> b(n), c(n);
+	map<int, int> m;
+	for (int i = 0,j; i < n; i++) {
+		cin >> j;
+		m[j]++;
+	}
+	for (int& i : b) cin >> i;
+	for (int& i : c) cin >> i;
+	
+	ll ans = 0;
+	for (int i = 0; i < n; i++) {
+		ans += m[b[c[i] - 1]];
+	}
+
+	cout << ans << '\n';
+}
