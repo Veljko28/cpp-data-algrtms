ccc0ba14225ea2877cb7ad71e1cae3a041351d24
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-03-04 21:01:53 +0100

    Create PrefixAndSuffixArray.cpp

---
+++ codeforces/PrefixAndSuffixArray.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    set<string> st;
+    for (int i = 0; i < (n - 1) * 2; i++) {
+        string s;
+        cin >> s;
+        string a = s;
+        reverse(all(a));
+        if (st.find(a) != st.end()) st.erase(a);
+        else st.insert(s);
+    }
+    if (st.size()) cout << "NO\n";
+    else cout << "YES\n";
+}
+
+
+int main() {
+    sync;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
