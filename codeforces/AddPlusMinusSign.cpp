ffe3e679031cb306f637f23620f5ee4139dc6ed9
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-03-09 07:57:20 +0100

    Create AddPlusMinusSign.cpp

---
+++ codeforces/AddPlusMinusSign.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string ans;
+    int a = s[0] == '1';
+    for (int i = 0; i < n-1; i++) {
+        if (s[i + 1] == '0') ans += "+";
+        else if (s[i + 1] == '1' and a >= 0) {
+            ans += '-';
+            a--;
+        }
+        else {
+            ans += '+';
+            a++;
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    sync;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
