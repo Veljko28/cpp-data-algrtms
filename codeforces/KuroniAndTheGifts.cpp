0b4ce50d31583534be88b9e02abaa8bcba77f727
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-03-17 07:59:03 +0100

    Create KuroniAndTheGifts.cpp

---
+++ codeforces/KuroniAndTheGifts.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int& i : a) cin >> i;
+    for (int& i : b) cin >> i;
+    sort(all(a));
+    sort(all(b));
+    for (int i : a) cout << i << " ";
+    cout << "\n";
+    for (int i : b) cout << i << " ";
+    cout << "\n";
+}
+
+int main() {
+    sync;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
