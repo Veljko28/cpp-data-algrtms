f79e7e839e8f12faecc0cac92f50431b974107fa
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-03-25 21:33:19 +0100

    Create ServalAndMochasArray.cpp

---
+++ codeforces/ServalAndMochasArray.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int& i : a) cin >> i;
+    bool ok = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int g = __gcd(a[j], a[i]);
+            if (g <= 2) {
+                ok = 1;
+                break;
+            }
+        }
+        if (ok) break;
+    }
+    if (ok) cout << "Yes\n";
+    else cout << "No\n";
+}
+
+
+int main() {
+    sync;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
