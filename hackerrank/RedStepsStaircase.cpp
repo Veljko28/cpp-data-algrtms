b8a547a859264c53b87451eabacb8020d96dff90
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-04-10 13:04:25 +0200

    Create RedStepsStaircase.cpp

---
+++ hackerrank/RedStepsStaircase.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+
+void solve() {
+    ll n;
+    int k;
+    cin >> n >> k;
+    vector<int> redStep(n);
+    for (int& i : redStep) cin >> i;
+    vector<ll> dp(k);
+    dp[0] = 1;
+    for (ll i = 1; i <= n; i++) {
+        for (int j = 0; j < k; j++) {
+            if ((i - j) < 0) break;
+            if (redStep[i - 1]) dp[i % k] = 0;
+            else dp[i % k] += dp[(i - j) % k];
+        }
+    }
+    cout << dp[n % k] << '\n';
+}
+
+int main() {
+    sync;
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
