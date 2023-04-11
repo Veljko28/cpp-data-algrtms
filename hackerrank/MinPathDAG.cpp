f54e46d1762f210eb719d5104d433e91ca631648
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-04-11 21:12:27 +0200

    Create MinPathDAG.cpp

---
+++ hackerrank/MinPathDAG.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<vector<int>> edges(n, vector<int>(n));
+    for (vector<int>& v : edges) {
+        for (int& i : v) {
+            cin >> i;
+        }
+    }
+    vector<int> dp(n, INT_MAX);
+    dp[n - 1] = 0;
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j = 0; j <= n; j++) {
+            if (edges[i][j]) {
+                dp[i] = min(dp[i], edges[i][j] + dp[j]);
+            }
+        }
+    }
+    cout << dp[0] << '\n';
+}
+
+int main() {
+    sync;
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
