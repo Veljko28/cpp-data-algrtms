388e8b8dcc75295a8c0fc43d937ca282b00af610
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-04-12 22:01:11 +0200

    Create LCS.cpp

---
+++ hackerrank/LCS.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    string a, b;
+    cin >> a >> b;
+    vector<vector<int>> dp(n+1, vector<int>(m+1));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (a[i - 1] == b[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+
+        }
+    }
+
+    cout << dp[n][m] << '\n';
+}
+
+int main() {
+    sync;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
