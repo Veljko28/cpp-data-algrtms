9eb163ddce06c1b16177a28de3baf865ef80cb7b
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-04-13 13:02:18 +0200

    Create MaximumLengthPairChain.cpp

---
+++ hackerrank/MaximumLengthPairChain.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].first >> a[i].second;
+    }
+    int mx = 1;
+    vector<int> lcp(n, 1);
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (a[i].first > a[j].second) {
+                lcp[i] = max(lcp[i], lcp[j] + 1);
+                mx = max(mx, lcp[i]);
+            }
+        }
+    }
+    cout << mx << '\n';
+}
+
+int main() {
+    sync;
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
