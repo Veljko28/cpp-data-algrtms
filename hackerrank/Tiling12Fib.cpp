2f865d3e0521c13ddba9a2231f381775e2b9fe73
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-04-14 10:52:18 +0200

    Create Tiling12Fib.cpp

---
+++ hackerrank/Tiling12Fib.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+
+
+void solve() {
+    int n;
+    cin >> n;
+    ll prev1 = 0, prev2 = 1;
+    if (n <= 2) {
+        cout << n << '\n';
+        return;
+    }
+    for (int i = 1; i < n; i++) {
+        ll curr = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = curr;
+    }
+    cout << prev2 << '\n';
+}
+
+
+int main() {
+    sync;
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
