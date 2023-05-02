a042ad059a0e5c044d8bff89a68af469ad132acf
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-05-02 21:24:33 +0200

    Create Stones.cpp

---
+++ AtCoder DP Edu/Stones.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> stones(n);
+    for (int& i : stones) cin >> i;
+    vector<bool> dp(k + 1);
+    for (int i = 0; i <= k; i++) {
+        for (int j : stones) {
+            if (i - j >= 0 and !dp[i - j]) {
+                dp[i] = 1;
+            }
+        }
+    }
+    cout << (dp[k] ? "First\n" : "Second\n");
+}
+
+
+int main() {
+    sync;
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+}
