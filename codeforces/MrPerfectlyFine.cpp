10d800cd26163e3796618ce0c074dcabdd14f76d
Вељко Mиловановић <71782085+Veljko28@users.noreply.github.com>
2023-05-06 22:51:20 +0200

    Create MrPerfectlyFine.cpp

---
+++ codeforces/MrPerfectlyFine.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <cstring>
+#include <ctime>
+#include <set>
+#include <list>
+#include <sstream>
+#include <iomanip>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <chrono>
+#include <sstream>
+#include <ostream>
+#include <assert.h>
+#include <cstdint>
+#include <climits>
+#define ll long long
+#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265358979323846 
+#define all(c) (c).begin(), (c).end()
+#define min_heap_pq priority_queue<int,vector<int>,greater<int>>
+using namespace std;
+
+
+void solve() {
+    int n;
+    cin >> n;
+    map<string, ll> mp;
+    mp["01"] = INT_MAX;
+    mp["11"] = INT_MAX;
+    mp["10"] = INT_MAX;
+
+    while (n--) {
+        ll m;
+        string s;
+        cin >> m >> s;
+        if (s == "00") continue;
+        mp[s] = min(mp[s], m);
+    }
+    ll ans = INT_MAX;
+    ans = min(ans, mp["11"]);
+    ans = min(ans, mp["01"] + mp["10"]);
+    if (ans >= INT_MAX) cout << "-1\n";
+    else cout << ans << '\n';
+}
+
+int main() {
+    sync;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
